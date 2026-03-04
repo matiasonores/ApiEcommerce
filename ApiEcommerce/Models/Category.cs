867ba85e87afe98bd788cf2e1a5d70d7ea6be9b7
Matias Onores <matiasonores@integracomex.com.ar>
2026-03-04 08:32:52 -0300

    added category model

---
+++ ApiEcommerce/Models/Category.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Category
+{
+    [Key]
+    public int Id {get;set;}
+    [Required]
+    public string Name {get;set;} = string.Empty;
+    public DateTime CreationDate {get;set;}
+}
