31746d7613bdcfb07b83565fd6630d82925686d3
Matias Onores <matiasonores@integracomex.com.ar>
2026-03-05 17:04:29 -0300

    added automapper

---
+++ ApiEcommerce/Mapping/CategoryProfile.cs
@@ -0,0 +1,10 @@
+using AutoMapper;
+
+public class CategoryProfile : Profile
+{
+    public CategoryProfile()
+    {
+        CreateMap<Category,CategoryDto>().ReverseMap();
+        CreateMap<Category,CreateCategoryDto>().ReverseMap();
+    }
+}
