b8202448ed2bc6a3539f416ee0add1d5c304e70a
Matias Onores <matiasonores@integracomex.com.ar>
2026-03-06 17:45:06 -0300

    added product model

---
+++ ApiEcommerce/Models/Product.cs
@@ -0,0 +1,26 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Product
+{
+    [Key]
+    public int ProductId { get; set; }
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    [Range(0, double.MaxValue)]
+    [Column(TypeName ="decimal(18,2)")]
+    public decimal Price { get; set; }
+    public string ImgUrl { get; set; } = string.Empty;
+    [Required]
+    public string SKU { get; set; } = string.Empty;
+    [Range(0, int.MaxValue)]
+    public int Stock { get; set; }
+    public DateTime CreationDate { get; set; } = DateTime.Now;
+    public DateTime? UpdateDate { get; set; } = null;
+    //Relation with Category
+    public int CategoryId { get; set; }
+    [ForeignKey("CategoryId")]
+    public required Category Category { get; set; }
+}
